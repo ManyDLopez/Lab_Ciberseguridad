b73ae4041ce9798ca334906011cb4047765e97f5
ManyDLopez <72290189+ManyDLopez@users.noreply.github.com>
2020-12-03 22:12:13 -0600

    Add files via upload

---
+++ 4_PowerShell.ps1
@@ -0,0 +1,8 @@
+﻿$puertos = Get-NetTCPConnection
+foreach ($elemento in $puertos){
+	if ($elemento.RemotePort -eq "80") {
+		Write-Host $elemento.LocalAddress "is" $elemento.RemotePort -ForegroundColor yellow
+	} elseif ($elemento.RemotePort -eq "443") {
+		Write-Host $elemento.LocalAddress "is" $elemento.RemotePort -ForegroundColor Red
+	}
+}
